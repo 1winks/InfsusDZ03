ef6fadad757d1d43718dff3cfb0bbd6931d9381f
Sven Winkler <sven.winkler@fer.hr>
2026-05-16 21:08:40 +0200

    kostur modela

---
+++ Backend/pinsus/src/main/java/infsus/pinsus/controller/responses/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package infsus.pinsus.controller.responses;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(SystemException.class)
+    public ResponseEntity<Map<String, String>> handleException(SystemException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
---
+++ Backend/pinsus/src/main/java/infsus/pinsus/controller/responses/SystemException.java
@@ -0,0 +1,11 @@
+package infsus.pinsus.controller.responses;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class SystemException extends RuntimeException {
+    public SystemException(String message) {
+        super(message);
+    }
+}
